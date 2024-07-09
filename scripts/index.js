fec256aace93bcbcc18a223afdce4572282ac770
Matvey <matveyalekseyevich@yandex.ru>
2024-07-09 18:54:45 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCallback) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__title').innerText = cardData.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        deleteCallback(cardElement);
+    });
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+document.addEventListener('DOMContentLoaded', () => {
+    initialCards.forEach(cardData => {
+        const cardElement = createCard(cardData, deleteCard);
+        placesList.appendChild(cardElement);
+    });
+});
